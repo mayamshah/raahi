b1fc7ffdffc6a05fe16fe2f44d16cc5518d7bcc4
Maya Shah <mayashah@Mayas-MacBook-Pro-3.local>
2020-05-26 08:00:37 -0700

    initial request

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "net/http"
+import "io/ioutil"
+
+const KEY = "AIzaSyB32cCcL4gD_WIYPP6dAVSprY_QYE3arsk"
+
+func main() {
+
+    str := "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key="
+    str += KEY
+    resp, err := http.Get(str)
+    if err != nil {
+    	}
+    response, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	fmt.Printf("%s",response)
+}
