4526a721db7226d983146afaead0034876100fb8
mayamshah <shahmmaya@gmail.com>
2020-05-29 07:53:07 -0700

    initial call to api for address coord

--- main.go
+++ main.go
@@ -2,20 +2,75 @@ package main
 
 import (
     "fmt"
+    "bufio"
+    "os"
+    "strings"
     "net/http"
     "io/ioutil"
+    "encoding/json"
+    // "strconv"
  )
 
-const KEY = "AIzaSyB32cCcL4gD_WIYPP6dAVSprY_QYE3arsk"
+const KEY = "&key=AIzaSyB32cCcL4gD_WIYPP6dAVSprY_QYE3arsk"
+const URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+
+func input_to_address(input string) {
+    arr := strings.Split(input," ")
+    var address string
+    for _, s := range arr {
+        address += "+" + s
+    }
+    address = strings.TrimPrefix(address, "+")
+    return address
+}
+
+func create_geo
 
 func main() {
 
-    str := "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key="
-    str += KEY
-    resp, err := http.Get(str)
+    //setup the scanner
+    scanner := bufio.NewScanner(os.Stdin)
+    fmt.Printf("Enter your address in the following format: Street Address, City, State\n")
+
+    //read user input
+    var input string
+    for scanner.Scan() {
+        input = scanner.Text()
+        if strings.Contains(input,"") { break }
+    }
+
+    //properly form the address for the api url    
+    address := input_to_address(input)
+
+
+    //create the api url and get the json response
+    url := URL + address + KEY
+    resp, err := http.Get(url)
     if err != nil {
-    	}
+        panic(err)
+    }
     response, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	fmt.Printf("%s",response)
+    resp.Body.Close() 
+
+
+    //get the latitude and longitude coordinates
+    var f interface{}
+    json.Unmarshal(response, &f)   
+    m := f.(map[string]interface{})
+    f = m["results"]
+    n := f.([]interface{})
+    o := n[0]
+    g := o.(map[string]interface{})
+    f = g["geometry"]
+    j := f.(map[string]interface{})
+    f = j["location"]
+    i := f.(map[string]interface{})
+    lat := i["lat"]
+    lon := i["lng"]
+    
+    fmt.Printf(lat)
+    fmt.Printf(lon)
+
+
+
 }
