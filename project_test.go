811593f42a42cddca66327d1f107965976d1d718
mayamshah <shahmmaya@gmail.com>
2020-06-08 21:24:35 -0700

    ran initial tests (#15) close #10

--- project_test.go
+++ project_test.go
@@ -10,7 +10,21 @@ func TestStraightLine(t *testing.T) {
 		distance string
 		error_fix float64
 	}{
-		{`12543 Palmtag Drive, Saratoga, CA`, `1`, .8},
+
+////////////// DO NOT RUN THESE TESTS - THEY HAVE ALREADY BEEN RUN /////////////////
+////////////// THE OUTPUT IS LOCATED IN THE REPO //////////////////////////////////
+		
+		// //suburban
+		// {`12543 Palmtag Drive, Saratoga, CA`, `1`, 1},
+		// {`327 Westwood Ln, Stockton, CA 95207`,`1`, 1},
+		// {`1511 Brook Valley Ct, Dallas, TX 75232`,`1`, 1},
+		// {`627 N Jackson St, Arlington, VA 22201`,`1`, 1},
+
+		// //city
+		// {`1050 Fell St, San Francisco, CA 94117`, `1`, 1},
+		// {`425 5th Ave, New York, NY 10016`, `1`, 1},
+		// {`1037 Chestnut St, Philadelphia, PA 1910`, `1`, 1},
+		// {`4551 SW 5th St, Coral Gables, FL 33134`,`1`, 1},
 
 	}
 		for _, test := range tests {
@@ -27,8 +41,22 @@ func TestSquareRoute(t *testing.T) {
 		distance string
 		error_fix float64
 	}{
-		{`12543 Palmtag Drive, Saratoga, CA`, `1`, .8},
 
+////////////// DO NOT RUN THESE TESTS - THEY HAVE ALREADY BEEN RUN /////////////////
+////////////// THE OUTPUT IS LOCATED IN THE REPO //////////////////////////////////
+
+		// //suburban
+		// {`12543 Palmtag Drive, Saratoga, CA`, `1`, 1},
+		// {`327 Westwood Ln, Stockton, CA 95207`,`1`, 1},
+		// {`1511 Brook Valley Ct, Dallas, TX 75232`,`1`, 1},
+		// {`627 N Jackson St, Arlington, VA 22201`,`1`, 1},
+
+		// //city
+		// {`1050 Fell St, San Francisco, CA 94117`, `1`, 1},
+		// {`425 5th Ave, New York, NY 10016`, `1`, 1},
+		// {`1037 Chestnut St, Philadelphia, PA 1910`, `1`, 1},
+		// {`4551 SW 5th St, Coral Gables, FL 33134`,`1`, 1},
+    
 	}
 		for _, test := range tests {
 		success, percent_error := execute(test.address, test.distance, square_route, test.error_fix) 
