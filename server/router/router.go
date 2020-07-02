fa73d4a5d375c2cf2b74a8abf4e5b4af778281b5
Maya Shah <shahmmaya@gmail.com>
2020-07-02 08:53:39 -0700

    updates

---
+++ server/router/router.go
@@ -0,0 +1,15 @@
+package router 
+
+import (
+	app "m20project.com/m/app"
+	"github.com/gorilla/mux"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/execute", app.Execute).Methods("POST", "OPTIONS")
+	return router
+}
