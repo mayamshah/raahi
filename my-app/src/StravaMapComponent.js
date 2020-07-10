c96a1583aa48da0205da44f7a916b877f305abc4
Maya Shah <shahmmaya@gmail.com>
2020-07-09 23:43:45 -0700

    some progress

---
+++ my-app/src/StravaMapComponent.js
@@ -0,0 +1,68 @@
+/*global google*/
+import React from 'react'
+import  { compose, withProps, lifecycle } from 'recompose'
+import {withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer} from 'react-google-maps'
+
+function createWayPoints(org) {
+
+    var waypoints = []
+    var i = 0
+    for (i = 0; i < 23*2; i = i + 2) {
+      waypoints.push({location: new google.maps.LatLng(org[i], org[i+1]), stopover: false})
+    }
+
+    console.log(waypoints.length)
+    return waypoints
+}
+
+class StravaMapComponent extends React.Component {
+  constructor(props){
+    super(props)
+  }
+render() {
+    const DirectionsComponent = compose(
+      withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyB32cCcL4gD_WIYPP6dAVSprY_QYE3arsk",
+        loadingElement: <div style={{ height: `400px` }} />,
+        containerElement: <div style={{ width: `100%` }} />,
+        mapElement: <div style={{height: `600px`, width: `600px` }}  />,
+        path: this.props.path,
+        start: this.props.start,
+        end: this.props.end
+      }),
+      withScriptjs,
+      withGoogleMap,
+      lifecycle({
+        componentDidMount() { 
+          const DirectionsService = new google.maps.DirectionsService();
+          DirectionsService.route({
+            origin: new google.maps.LatLng(this.props.start[0], this.props.start[1]),
+            destination: new google.maps.LatLng(this.props.end[0], this.props.end[1]),
+            waypoints: createWayPoints(this.props.path),
+            travelMode: google.maps.TravelMode.WALKING,
+          }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+this.setState({
+                directions: {...result},
+                markers: true
+              })
+            } else {
+              console.error(`error fetching directions ${result}`);
+            }
+          });
+        }
+      })
+    )(props =>
+      <GoogleMap
+        defaultZoom={3}
+      >
+        {props.directions && <DirectionsRenderer directions={props.directions} suppressMarkers={props.markers}/>}
+      </GoogleMap>
+    );
+return (
+        <DirectionsComponent
+        />
+    )
+  }
+}
+export default StravaMapComponent
